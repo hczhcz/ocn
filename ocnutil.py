c8359ee940cfd7fec84d9e7f123d50ffc1c1e6ff
hcz <h.cz@qq.com>
2015-03-19 08:52:31 +0800

    init

---
+++ ocnutil.py
@@ -0,0 +1,78 @@
+# import os, time, xml
+import string
+import MySQLdb
+
+
+def fmtname(name):
+    '''convert "Abcd1234 ()?_" to "Abcd1234_____"'''
+
+    charset = string.ascii_letters + string.digits
+    return ''.join((
+        i if i in charset else '_' for i in name
+    ))
+
+
+def fmttype(name):
+    '''convert ("char", 3) to "char(3)" or "int" to "int"'''
+
+    if type(name) == str:
+        return fmtname(name)
+    else:
+        return fmtname(name[0]) + '(' + str(int(name[1])) + ')'
+
+
+class OcnDBTask(object):
+    def __init__(
+        self,
+        host='localhost',
+        port=3306,
+        db='ocnutilpy',
+        user='ocnutilpy',
+        passwd='ocnutilpy',
+        table='data',
+        charset='utf8',
+    ):
+        self.conn = MySQLdb.connect(
+            host=host,
+            port=port,
+            db=db,
+            user=user,
+            passwd=passwd,
+            charset=charset,
+        )
+        self.cursor = self.conn.cursor()
+        self.table = table
+
+    def chktable(self, colname, coltype):
+        '''create a table with a column if it does not exist'''
+
+        self.cursor.execute('''
+            create table if not exists %s(%s %s);
+        ''' % (
+            fmtname(self.table),
+            fmtname(colname),
+            fmttype(coltype),
+        ))
+
+    def chkcol(self, colname, coltype):
+        pass
+
+    def __del__(self):
+        self.conn.commit()
+        self.cursor.close()
+        self.conn.close()
+
+
+# class OcnDBLoadTask(OcnDBTask):
+#     def __init__():
+#         super(OcnDBTask, self)
+
+
+def loadfile(path):
+    '''read file as a string'''
+    return open(path, 'r').read()
+
+
+if __name__ == '__main__':
+    task = OcnDBTask(table='test1')
+    task.chktable('col1', ('char', 123))
