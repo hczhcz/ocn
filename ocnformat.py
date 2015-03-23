4a3205145dc251a6b9b01d5875fceca25ae9ace0
hcz <h.cz@qq.com>
2015-03-23 19:52:42 +0800

    add ocnformat.py

---
+++ ocnformat.py
@@ -0,0 +1,18 @@
+table_db = 'dbtest2'
+
+columns_db = (
+    ('STB_NO', ('char', 17)),
+    ('SERVICE_CODE', 'int'),
+    ('PROVIDER', ('varchar', 64)),
+    ('ASSET_NAME', ('varchar', 64)),
+    ('CONTENT_NAME', ('varchar', 64)),
+    ('RENTAL_TIME', 'datetime'),
+    ('RENTAL_EXPIRE_TIME', 'datetime'),  # may be unavaliable
+)
+
+primaries_db = (
+    'STB_NO',
+    # 'SERVICE_CODE', 'PROVIDER',
+    # 'ASSET_NAME', 'CONTENT_NAME',
+    'RENTAL_TIME', 'RENTAL_EXPIRE_TIME'
+)
